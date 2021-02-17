ed65684e69f2fe4e34fa00d4da14f29ffb6e7bd8
Andrés <pixelbiff@gmail.com>
2021-02-17 02:41:27 -0600

    Cambios visuales
    Cambios en  en carpetas, breadcrumbs, se agregan imágenes de nueva identidad

---
+++ application/logs/log-2021-02-16.php
@@ -0,0 +1,181 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-02-16 20:20:39 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-16 20:20:39 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-16 20:20:39 --> Query error: Expression #38 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'redsems.categorias_objetos.TIPO' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT *
+FROM `archivos`
+JOIN `categorias_objetos` ON `categorias_objetos`.`ID_OBJETO` = `archivos`.`ID`
+WHERE   (
+`archivos`.`TITULO` LIKE '%buscar%' ESCAPE '!'
+OR  `archivos`.`DESCRIPCION` LIKE '%buscar%' ESCAPE '!'
+ )
+AND   (
+`archivos`.`ESTADO` = 'activo'
+AND `categorias_objetos`.`ID_CATEGORIA` = '0'
+ )
+GROUP BY `archivos`.`ID`
+ORDER BY `TITULO` ASC
+ERROR - 2021-02-16 20:20:39 --> Severity: error --> Exception: Call to a member function result() on bool /Applications/MAMP/htdocs/red_sems/application/models/GeneralModel.php 108
+ERROR - 2021-02-16 20:21:01 --> Query error: Expression #38 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'redsems.categorias_objetos.TIPO' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT *
+FROM `archivos`
+JOIN `categorias_objetos` ON `categorias_objetos`.`ID_OBJETO` = `archivos`.`ID`
+WHERE   (
+`archivos`.`TITULO` LIKE '%buscar%' ESCAPE '!'
+OR  `archivos`.`DESCRIPCION` LIKE '%buscar%' ESCAPE '!'
+ )
+AND   (
+`archivos`.`ESTADO` = 'activo'
+AND `categorias_objetos`.`ID_CATEGORIA` = '0'
+ )
+GROUP BY `archivos`.`ID`
+ORDER BY `TITULO` ASC
+ERROR - 2021-02-16 20:21:01 --> Severity: error --> Exception: Call to a member function result() on bool /Applications/MAMP/htdocs/red_sems/application/models/GeneralModel.php 108
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:58 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:59 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:59 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:59 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:59 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:59 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:59 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:59 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:21:59 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:00 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:00 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:00 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:29 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:29 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:30 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:31 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:31 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:31 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:22:31 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:23:14 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-16 20:23:14 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-16 20:23:14 --> Query error: Expression #38 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'redsems.categorias_objetos.TIPO' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT *
+FROM `archivos`
+JOIN `categorias_objetos` ON `categorias_objetos`.`ID_OBJETO` = `archivos`.`ID`
+WHERE   (
+`archivos`.`ESTADO` = 'activo'
+AND `categorias_objetos`.`ID_CATEGORIA` = '0'
+ )
+GROUP BY `archivos`.`ID`
+ORDER BY `TITULO` ASC
+ERROR - 2021-02-16 20:23:14 --> Severity: error --> Exception: Call to a member function result() on bool /Applications/MAMP/htdocs/red_sems/application/models/GeneralModel.php 108
+ERROR - 2021-02-16 20:23:48 --> Query error: Expression #38 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'redsems.categorias_objetos.TIPO' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT *
+FROM `archivos`
+JOIN `categorias_objetos` ON `categorias_objetos`.`ID_OBJETO` = `archivos`.`ID`
+WHERE   (
+`archivos`.`ESTADO` = 'activo'
+AND `categorias_objetos`.`ID_CATEGORIA` = '0'
+ )
+GROUP BY `archivos`.`ID`
+ORDER BY `TITULO` ASC
+ERROR - 2021-02-16 20:23:48 --> Severity: error --> Exception: Call to a member function result() on bool /Applications/MAMP/htdocs/red_sems/application/models/GeneralModel.php 108
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:09 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:09 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:09 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:09 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:12 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:13 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:14 --> 404 Page Not Found: Assets/js
+ERROR - 2021-02-16 20:24:22 --> Query error: Expression #38 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'redsems.categorias_objetos.TIPO' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT *
+FROM `archivos`
+JOIN `categorias_objetos` ON `categorias_objetos`.`ID_OBJETO` = `archivos`.`ID`
+WHERE   (
+`archivos`.`ESTADO` = 'activo'
+AND `categorias_objetos`.`ID_CATEGORIA` = '0'
+ )
+GROUP BY `archivos`.`ID`
+ORDER BY `TITULO` ASC
+ERROR - 2021-02-16 20:24:22 --> Severity: error --> Exception: Call to a member function result() on bool /Applications/MAMP/htdocs/red_sems/application/models/GeneralModel.php 108
+ERROR - 2021-02-16 20:26:53 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-16 20:26:53 --> 404 Page Not Found: Assets/img
+ERROR - 2021-02-16 20:26:54 --> Query error: Expression #38 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'redsems.categorias_objetos.TIPO' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT *
+FROM `archivos`
+JOIN `categorias_objetos` ON `categorias_objetos`.`ID_OBJETO` = `archivos`.`ID`
+WHERE   (
+`archivos`.`ESTADO` = 'activo'
+AND `categorias_objetos`.`ID_CATEGORIA` = '0'
+ )
+GROUP BY `archivos`.`ID`
+ORDER BY `TITULO` ASC
+ERROR - 2021-02-16 20:26:54 --> Severity: error --> Exception: Call to a member function result() on bool /Applications/MAMP/htdocs/red_sems/application/models/GeneralModel.php 108
